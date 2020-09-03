d7c652ac0e63a45bc38c3ecdbc4f5e86b5f6c59a
Ayako Iwasaki <a.iwasaki0128@gmail.com>
2020-09-04 03:38:50 +0900

    added mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @i
+    M = 1 // i = 1
+    @R2
+    M = 0
+(LOOP)
+    @i
+    D = M // D = i
+    @R1
+    D = D - M // D = D - R1 (i = i - R1)
+    @END
+    D;JGT // if D > 0, jump to END
+    @R0
+    D = M // D = R0
+    @R2
+    M = D + M // M = D + R0 (R2 = R0 + R2)
+    @i
+    M = M + 1 // i = i + 1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
