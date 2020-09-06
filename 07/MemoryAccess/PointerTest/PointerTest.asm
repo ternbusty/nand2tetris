eee704c593890a0ea401613868426d738bd43a37
Ayako Iwasaki <a.iwasaki0128@gmail.com>
2020-09-07 02:22:50 +0900

    added Pointer test

---
+++ 07/MemoryAccess/PointerTest/PointerTest.asm
@@ -0,0 +1,169 @@
+@3030
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@0
+D = A
+@3
+D = A + D
+@R13
+M = D
+@SP
+A = M
+D = M
+@R13
+A = M
+M = D
+@3040
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@1
+D = A
+@3
+D = A + D
+@R13
+M = D
+@SP
+A = M
+D = M
+@R13
+A = M
+M = D
+@32
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@2
+D = A
+@THIS
+D = M + D
+@R13
+M = D
+@SP
+A = M
+D = M
+@R13
+A = M
+M = D
+@46
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@6
+D = A
+@THAT
+D = M + D
+@R13
+M = D
+@SP
+A = M
+D = M
+@R13
+A = M
+M = D
+@0
+D = A
+@3
+A = A + D
+D = M
+@SP
+A = M
+M = D
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@1
+D = A
+@3
+A = A + D
+D = M
+@SP
+A = M
+M = D
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+A = A - 1
+M = M + D
+@2
+D = A
+@THIS
+A = M + D
+D = M
+@SP
+A = M
+M = D
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+A = A - 1
+M = M - D
+@6
+D = A
+@THAT
+A = M + D
+D = M
+@SP
+A = M
+M = D
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+A = A - 1
+M = M + D
